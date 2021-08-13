9d78c77d538be59515abed2d7dec2bb000ae4e52
algisju <69957906+algisju@users.noreply.github.com>
2021-08-13 03:05:09 -0400

    Add files via upload

---
+++ plotvid.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+
+capture = cv.VideoCapture('/dev/video0')
+
+while True:
+    isTrue,frame = capture.read()
+    cv.imshow('Video',frame)
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
