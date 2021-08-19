b0749b7004c22ffca7bff475dd5f976366f31546
algisju <69957906+algisju@users.noreply.github.com>
2021-08-19 05:51:55 -0400

    Add files via upload

---
+++ qip.py
@@ -0,0 +1,4 @@
+from netifaces import interfaces, ifaddresses, AF_INET
+for ifaceName in interfaces():
+    addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':'No IP addr'}] )]
+    print '%s: %s' % (ifaceName, ', '.join(addresses))
