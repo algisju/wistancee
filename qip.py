73b9ad5f42e5a5fe84f2e55381334208050dd051
algisju <69957906+algisju@users.noreply.github.com>
2021-08-24 05:50:28 -0400

    Delete qip.py

--- qip.py
+++
@@ -1,4 +0,0 @@
-from netifaces import interfaces, ifaddresses, AF_INET
-for ifaceName in interfaces():
-    addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':'No IP addr'}] )]
-    print '%s: %s' % (ifaceName, ', '.join(addresses))
