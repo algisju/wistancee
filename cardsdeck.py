07941a5a53353cc55e227ecca16d6cf2b8e0caef
algisju <69957906+algisju@users.noreply.github.com>
2021-11-10 00:15:56 -0500

    Delete cardsdeck.py

--- cardsdeck.py
+++
@@ -1,8 +0,0 @@
-import itertools
-ranks=list(range(2,11))+['J','Q','K','A']
-ranks=[str(rank) for rank in ranks]
-suits=['Hearts','Clubs','Diamonds','Spades']
-deck=[card for card in itertools.product(ranks,suits)]
-for (index,card) in enumerate(deck):
-	print(1+index,card)
-
