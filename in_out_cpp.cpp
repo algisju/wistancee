80fb3e2519be55a110e89ba5094d8b2d23ef39ff
algisju <69957906+algisju@users.noreply.github.com>
2021-11-25 09:14:28 +0100

    Add files via upload

---
+++ in_out_cpp.cpp
@@ -0,0 +1,13 @@
+// #include <Windows.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int s;
+	cout<<"Type your Shit => ";
+	cin>>s;
+    cout<<"There ===> "<<s<<endl;
+    cout<<"\U0001F412"<<endl;
+    
+}
