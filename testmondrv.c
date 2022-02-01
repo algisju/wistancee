73c95a08b93d9895f0dd1dbb365dee4458cd12c1
algisju <69957906+algisju@users.noreply.github.com>
2022-02-01 15:58:10 +0800

    Add files via upload

---
+++ testmondrv.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+// needs lvgl lib -- investigate 
+#define BUF_W 20
+#define BUF_H 10
+
+lv_color_t buf[BUF_W * BUF_H];
+lv_color_t * buf_p = buf;
+uint16_t x, y;
+for(y = 0; y &lt; BUF_H; y++) {
+    lv_color_t c = lv_color_mix(LV_COLOR_BLUE, LV_COLOR_RED, (y * 255) / BUF_H);
+    for(x = 0; x &lt; BUF_W; x++){
+        (*buf_p) =  c;
+        buf_p++;
+    }
+}
+
+lv_area_t a;
+a.x1 = 10;
+a.y1 = 40;
+a.x2 = a.x1 + BUF_W - 1;
+a.y2 = a.y1 + BUF_H - 1;
+my_flush_cb(NULL, &a, buf);
